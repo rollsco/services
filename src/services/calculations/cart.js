687a89787409379b5b2ca939582c6baee219ed00
Juan M <juan.martinez@districtm.net>
2020-01-04 15:30:51 -0500

    Add points from cart items calculation

--- src/services/calculations/cart.js
+++ src/services/calculations/cart.js
@@ -1,5 +1,14 @@
-export const getTotalPriceFromCartBeforeDiscount = cart =>
-cart.items.reduce((sum, { product }) => sum + product.price, 0);
+export const getTotalPriceFromCartBeforeDiscount = items =>
+  items.reduce((sum, { product }) => sum + product.price, 0);
 
-// export const getTotalPriceFromCartAfterDiscount = cart =>
-// cart.items.reduce((sum, { product }) => sum + product.price, 0);
+// export const getTotalPriceFromCartAfterDiscount = items =>
+//   items.reduce((sum, { product }) => sum + product.price, 0);
+
+export const calculateTotalCost = items =>
+  items.reduce((priceSum, item) => priceSum + item.totalPrice, 0);
+
+export const calculateTotalPoints = items => {
+  const totalCost = calculateTotalCost(items);
+
+  return Math.floor(totalCost / 1000);
+}
